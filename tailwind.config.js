dae3fe9f8469bc881ad9f9c9ef4a15a315e9e0f2
Ashek Nabil <asheknabil11@gmail.com>
2025-10-03 23:26:47 +0600

    Hero section complete

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+        'roboto-mono': ['Roboto Mono', 'monospace'],
+        josefin: ["Josefin Sans", 'sans-serif'],
+      }
+    }
+  }
+}
